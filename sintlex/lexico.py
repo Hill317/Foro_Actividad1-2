2128e2f4ee2c9c3d14d3764a60b9de6313a71d50
Hill317 <166350960+Hill317@users.noreply.github.com>
2024-10-06 06:36:12 -0600

    Add files via upload

--- sintlex/lexico.py
+++ sintlex/lexico.py
@@ -12,7 +12,8 @@ delimitador = {
     '(' : 'PABIERTO',
     ')' : 'PCERRADO',
     '{' : 'LABIERTO',
-    '}' : 'LCERRADO'
+    '}' : 'LCERRADO',
+    '"' : 'COMILLA',
 }
 
 operador = {
@@ -20,32 +21,35 @@ operador = {
     '+' : 'SUMA',
 }
 
-tokens = ['NUMERO', 'VARIABLE', 'IDENTIFICADOR', 'PUNTOCOMA', 'CADENA'] + list(reserved.values()) + list(delimitador.values()) + list(operador.values())
+signos = {
+    ';' : 'PUNTOCOMA',
+    ',' : 'COMA',
+}
+
+tokens = ['VARIABLE', 'IDENTIFICADOR', 'CADENA'] + list(reserved.values()) + list(delimitador.values()) + list(operador.values()) + list(signos.values())
 
 t_ignore = ' \t\r'
 
-
-def t_CADENA(t):
-    r'[^\"]+'
-    t.type = 'CADENA'
+def t_IDENTIFICADOR(t):
+    r'Suma'
     return t
 
 def t_VARIABLE(t):
-    r'[a-z]'
+    r'\w+'
     if t.value in reserved:
         t.type = reserved[t.value]
     else:
-        t.type = 'VARIABLE'
-    return t
-
-def t_IDENTIFICADOR(t):
-    r'suma'
+        if len(t.value) == 1:
+            t.type = 'VARIABLE'
+        else:
+            t.type = 'CADENA'
     return t
 
 t_PABIERTO = r'\('
 t_PCERRADO = r'\)'
 t_LABIERTO = r'\{'
 t_LCERRADO = r'\}'
+t_ignore_COMILLA = r'"'
 t_IGUAL = r'='
 t_SUMA = r'\+'
 
@@ -53,30 +57,32 @@ def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value)
 
-t_NUMERO = r'\d+'
 t_PUNTOCOMA = r';'
+t_ignore_COMA = r','
 
 def t_error(t): 
     print('Caracter no valido',t.value[0])
     t.lexer.skip(1)
 
-lexer = lex.lex()
+def crear_lexer():
+    lexer = lex.lex()
+    return lexer
 
-data = '''
-programa suma(){\
-    int a,b,c;
-    read a;
-    read b;
-    c=a+b;
-printf ("la suma es");
-end;
-}
-'''
+# data = '''
+# programa Suma(){\
+#     int a,b,c;
+#     read a;
+#     read b;
+#     c=a+b;
+# printf ("la suma es");
+# end;
+# }
+# '''
 
-lexer.input(data)
+# lexer.input(data)
 
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
-    print(tok.type, tok.value)
+# while True:
+#     tok = lexer.token()
+#     if not tok: 
+#         break      # No more input
+#     print(tok.type, tok.value)
